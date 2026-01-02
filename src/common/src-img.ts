ad5e8bd8c5e74b5ccdba0f239ef66a710f1a02b6
vkordash <vkordash@gmail.com>
2026-01-02 16:19:13 +0200

    return src

--- src/common/src-img.ts
+++ src/common/src-img.ts
@@ -4,6 +4,6 @@ export function getSrc(src:string, SiteUrl:any){
         if (src.substr(0,2)=="./") {
             return  SiteUrl+src.substr(2); 
         }
-
+        return src;
         return 'http://192.168.77.253/test_docs/nest/nest_cms/src/'+src;
     }
